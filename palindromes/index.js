493077242aa21f181d61d3629120c6b081d94121
Bane Debeljevic <bane.dd@gmail.com>
2019-10-29 00:02:00 -0400

    Simplifying the code to just compare the string to its own reverse

--- palindromes/index.js
+++ palindromes/index.js
@@ -8,8 +8,6 @@ module.exports.isPalindrome = str => {
   // We want the comparison to be case-insensitive and to only include alphanumeric characters
   str = str.replace(/[^0-9a-zA-Z]/g, "").toLowerCase();
 
-  const midpoint = Math.floor(str.length / 2);
-  const secondHalf = str.substring(str.length % 2 === 0 ? midpoint : midpoint + 1);
-  const flippedSecondHalf = secondHalf.split("").reduce((cur, acc) => acc + cur);
-  return str.substring(0, midpoint) === flippedSecondHalf;
+  // NOTE: We could reverse the string in multiple ways, including using Array.reverse and various loops
+  return str.split("").reduce((cur, acc) => acc + cur) === str;
 };
